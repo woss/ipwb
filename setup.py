b2ef0affef63adc2c7d26ee57b0e978f59a996c2
Mat Kelly (machawk1) <me@matkelly.com>
2016-05-10 12:58:44 -0400

    Fix setup README ref to rst instead of md

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 from ipwb import __version__
 
-long_description = open('README.md').read()
+long_description = open('README.rst').read()
 
 setup(
     name='ipwb',
