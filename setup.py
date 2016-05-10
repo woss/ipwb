7d153c92e30873c63c57832ef03a70535b864367
Mat Kelly (machawk1) <me@matkelly.com>
2016-05-10 13:11:52 -0400

    Rm cruft, add setup metadata

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     name='ipwb',
     version=__version__,
     url='https://github.com/oduwsdl/ipwb',
+    download_url="https://github.com/oduwsdl/ipwb",
     author='Mat Kelly',
     author_email='mkelly@cs.odu.edu',
     description='InterPlanetary Wayback (ipwb): Web Archive integration with IPFS',
@@ -23,14 +24,20 @@ setup(
       'surt',
       'ipfs-api'
     ],
+    keywords='http web archives ipfs distributed odu wayback',
     classifiers=[
       'Development Status :: 2 - Pre-Alpha',
+      
       'Environment :: Web Environment',
+      
       'Programming Language :: Python :: 2.7',
+      
       'License :: OSI Approved :: MIT License',
+      
       'Intended Audience :: Developers',
       'Intended Audience :: Information Technology',
       'Intended Audience :: Science/Research',
+      
       'Topic :: Internet :: WWW/HTTP',
       'Topic :: System :: Archiving',
       'Topic :: System :: Archiving :: Backup',
