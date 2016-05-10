2615ff9f071c574e2b64e2f623f622fdd54e1a1d
Mat Kelly (machawk1) <me@matkelly.com>
2016-05-10 12:53:35 -0400

    Add classifiers to setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='https://github.com/oduwsdl/ipwb',
     author='Mat Kelly',
     author_email='mkelly@cs.odu.edu',
-    description='Web Archive integration with IPFS',
+    description='InterPlanetary Wayback (ipwb): Web Archive integration with IPFS',
     packages=['ipwb',],
     license='MIT',
     long_description=long_description,
@@ -24,6 +24,16 @@ setup(
       'ipfs-api'
     ],
     classifiers=[
-    
+      'Development Status :: 2 - Pre-Alpha',
+      'Environment :: Web Environment',
+      'Programming Language :: Python :: 2.7',
+      'License :: OSI Approved :: MIT License',
+      'Intended Audience :: Developers',
+      'Intended Audience :: Information Technology',
+      'Intended Audience :: Science/Research',
+      'Topic :: Internet :: WWW/HTTP',
+      'Topic :: System :: Archiving',
+      'Topic :: System :: Archiving :: Backup',
+      'Topic :: Utilities',
     ]
 )
