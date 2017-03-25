d61e234963191b0f451c5676e4cdb874dc9420a6
Mat Kelly <me@matkelly.com>
2017-03-24 23:07:40 -0400

    Fix standardJS issues, prep for merge

--- ipwb/serviceWorker.js
+++ ipwb/serviceWorker.js
@@ -1,44 +1,49 @@
+/* eslint-env serviceworker */
+
 console.log('Loading ServiceWorker.')
 
 var baseDatetime = ''
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   console.log('Installing ServiceWorker.')
 })
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   console.log('Activating ServiceWorker.')
 })
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   console.log('Fetch event triggered.')
   console.log(event.request)
   var request = event.request
-  if (event.request.mode != 'navigate' && 
-      event.request.url.indexOf('/webui/') === -1) // Do not rewrite webui embedded resources
-  {
-       request = reroute(event.request, basedatetime) // Only embedded resources
-       console.log('REROUTING request for ' + event.request.url + ' to ' + request.url)
 
+  // TODO: consult the referrer header on each request instead of using a global var
+  if (event.request.mode !== 'navigate' &&
+      event.request.url.indexOf('/webui/') === -1) { // Do not rewrite webui embedded resources
+       // TODO: use a 3XX redirect to better guide the browser
+       //  if hostname == refferrer, check to ensure serviceworker does not run infinitely on each embedded resource
+    request = reroute(event.request, baseDatetime) // Only embedded resources
+    console.log('REROUTING request for ' + event.request.url + ' to ' + request.url)
+    console.log(event.request)
   } else if (event.request.mode === 'navigate') {
-      // We need to preserve memento-datetime for use in requesting 
-      //  embedded resources. Possibly use SW caches instead? TODO
-      var datetime = event.request.url.match(/\/([0-9]{14})\//)[1]
-      basedatetime = datetime
+    // We need to preserve memento-datetime for use in requesting
+    //  embedded resources. Possibly use SW caches instead? TODO
+    var datetime = event.request.url.match(/\/([0-9]{14})\//)[1]
+    baseDatetime = datetime
   }
 
   event.respondWith(
     fetch(request).then(serverFetch, serverFailure).catch(serverFailure)
   )
 
-  function serverFetch(response) {
+  function serverFetch (response) {
     console.log('Fetching from server.')
-    console.log(response);
-    return response;
+    console.log(response)
+    return response
   }
 
-  function serverFailure() {
-    console.log('Fetching from server failed.');
+  function serverFailure () {
+    console.log('Fetching from server failed.')
     return new Response('<h1>Service Unavailable</h1>', {
       status: 503,
       statusText: 'Service Unavailable',
@@ -48,7 +53,7 @@ self.addEventListener('fetch', function(event) {
     })
   }
 
-  function reroute(request, datetime) {
+  function reroute (request, datetime) {
     return new Request(`http://127.0.0.1:5000/memento/${datetime}/${request.url}`) // TODO: Rm hard-code for server
   }
 })
