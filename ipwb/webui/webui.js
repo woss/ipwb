ba8aaeffbc814e590eb252df9f058646caea6fdb
Mat Kelly (machawk1) <me@matkelly.com>
2017-06-29 13:25:10 -0400

    Rm debug msg

--- ipwb/webui/webui.js
+++ ipwb/webui/webui.js
@@ -100,7 +100,6 @@ function stopIPFSDaemon () {
 function getIPFSWebUIAddress () {
   var setIPFSWebUILink = function (resp) {
     document.getElementById('webui').setAttribute('href', 'http://' + resp)
-    console.log(resp)
   }
   var fail = function () { console.log('fail') }
   var err = function () { console.log('err') }
