37037ef412b846614e62cb4c73a169d6f010deb2
Mat Kelly <me@matkelly.com>
2017-12-12 19:12:46 -0500

    pep8 fix

--- ipwb/indexer.py
+++ ipwb/indexer.py
@@ -3,11 +3,11 @@
 """
 InterPlanetary Wayback indexer
 
-This script reads a WARC file and returns a CDXJ representative of its contents.
- In doing so, it extracts all archived HTTP responses from warc-response
- records, separates the HTTP header from the body, pushes each into IPFS, and
- retains the hashes. These hashes are then used to populate the JSON block
- corresponding to the archived URI.
+This script reads a WARC file and returns a CDXJ representative of its
+ contents. In doing so, it extracts all archived HTTP responses from
+ warc-response records, separates the HTTP header from the body, pushes each
+ into IPFS, and retains the hashes. These hashes are then used to populate the
+ JSON block corresponding to the archived URI.
 """
 
 from __future__ import print_function
