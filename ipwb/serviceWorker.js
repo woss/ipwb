236bc42dce6ef3750ce188981d665e2340b9ff40
Sawood Alam <ibnesayeed@gmail.com>
2017-12-18 12:42:01 -0500

    Utilizing the reconstructive module in the ServiceWorker

--- ipwb/serviceWorker.js
+++ ipwb/serviceWorker.js
@@ -1,76 +1,39 @@
 /* eslint-env serviceworker */
 
-let baseDatetime = ''
+// This makes a module available named "reconstructive"
+importScripts('https://oduwsdl.github.io/reconstructive/reconstructive.js')
 
-function getMyVersion () {
-  fetch(self.location.href)
-  .then(function (resp) {
-    console.log('Running ServiceWorker version ' + resp.headers.get('Server'))
-  })
-}
+// Customize configs (defaults should work for IPWB out of the box)
+// reconstructive.init({
+//   version: 'reconstructive.js:v1',
+//   urimPattern: self.location.origin + '/memento/<datetime>/<urir>',
+//   showBanner: false
+// })
 
+// Add any custom exclusions or modify or delete default ones
+//> reconstructive.exclusions
+//< {
+//<   notGet: f (event, config),
+//<   localResource: f (event, config)
+//< }
+
+// Pass a custom function to generate banner markup
+// reconstructive.bannerCreator(f (event, rewritten, config))
+// Or update the rewriting logic
+// reconstructive.updateRewriter(f (event, rewritten, config))
+
+// This is unnecessary, but can be useful for debugging or in future
 self.addEventListener('install', function (event) {
   console.log('Installing ServiceWorker.')
 })
 
+// This is unnecessary, but can be useful for debugging or in future
 self.addEventListener('activate', function (event) {
   console.log('Activating ServiceWorker.')
 })
 
-self.addEventListener('fetch', function (event) {
-  let request = event.request
-
-  const url = new URL(event.request.url)
-  const isNavigation = event.request.mode === 'navigate'
-  const isWebUI = event.request.url.indexOf('/webui/') !== -1
-  const isDaemon = event.request.url.indexOf('/daemon/') !== -1
-  const isConfig = event.request.url.indexOf('/config/') !== -1
-  const isReplayRoot = (url.pathname === '/' || url.pathname === '')
-  const isRootMemento = event.request.url === event.request.referrer
-  //var isAMemento = event.request.url.indexOf('/memento/') !== -1 || event.request.url.match('\/[0-9]{14}\/') !== null
-  const isAMemento = event.request.url.match('\/[0-9]{14}\/') !== null
-
-  let referrerDatetime = event.request.referrer.match(/\/([0-9]{14})\//)
-  if (referrerDatetime !== null) {
-    referrerDatetime = referrerDatetime[1]
-  }
-
-  if (isNavigation || isReplayRoot || isDaemon || isConfig) {
-    return // Internal asset, no SW needed
-  }
-
-  // TODO: consult the referrer header on each request instead of using a global var
-  //if ( event.request.url.split('/')[2] !== document.location.host) {
-  if (!isNavigation && !isWebUI && !isDaemon && !isRootMemento && !isConfig && !isAMemento) { // Do not rewrite webui embedded resources or daemon
-       // TODO: use a 3XX redirect to better guide the browser
-       //  if hostname == referrer, check to ensure serviceworker does not run infinitely on each embedded resource
-    request = reroute(event.request, referrerDatetime) // Only embedded resources
-    console.log('REROUTING request for ' + event.request.url + ' to ' + request.url)
-  }
-
-  event.respondWith(
-    fetch(request).then(serverFetch, serverFailure).catch(serverFailure)
-  )
-
-  function serverFetch (response) {
-    console.log('Fetching from server.')
-    getMyVersion()
-    return response
-  }
-
-  function serverFailure () {
-    console.log('Fetching from server failed.')
-    return new Response('<h1>Service Unavailable</h1>', {
-      status: 503,
-      statusText: 'Service Unavailable',
-      headers: new Headers({
-        'Content-Type': 'text/html'
-      })
-    })
-  }
-
-  function reroute (request, datetime) {
-    // TODO: Use replay host/ip and host here instead
-    return new Request(`http://localhost:5000/memento/${datetime}/${request.url}`) // TODO: Rm hard-code for server
-  }
+self.addEventListener("fetch", function(event) {
+  console.log('A fetch event triggered:', event);
+  // Add any custom logic here to conditionally call the reroute function
+  reconstructive.reroute(event);
 })
