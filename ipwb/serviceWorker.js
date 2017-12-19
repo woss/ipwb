d2d8b8ab533abfba0287900309ea5220c231c44f
Sawood Alam <ibnesayeed@gmail.com>
2017-12-18 21:07:04 -0500

    Revert "Adding exclusion for reconstructive.js loading"
    This reverts commit f158837d5152bf6b5c6e75ed45d22348fecc3a0f.
    
    This exclusion is not needed as the recursive loop was caused by the server

--- ipwb/serviceWorker.js
+++ ipwb/serviceWorker.js
@@ -16,9 +16,6 @@ importScripts('/reconstructive.js')
 //<   notGet: f (event, config),
 //<   localResource: f (event, config)
 //< }
-reconstructive.exclusions.selfScript = function (event, config) {
-  event.request.url.endsWith('reconstructive.js')
-}
 
 // Pass a custom function to generate banner markup
 // reconstructive.bannerCreator(f (event, rewritten, config))
