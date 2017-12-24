e5ab0147cdfd43e52445f680ea83db6aba91bc3e
Sawood Alam <ibnesayeed@gmail.com>
2017-12-23 22:43:50 -0500

    Utilize built-in mechanism for initialization when necessary

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,8 @@ set -e
 
 if [[ ("$1" = "ipwb") && ("$1" != "$@") && ("$@" != *" -h"*) && ("$@" != *" --help"*) ]]
 then
-    # Initialize IPFS if not initialized already
-    if [ ! -f $IPFS_PATH/config ]
-    then
-        ipfs init
-    fi
-    # Run the IPFS daemon in background
-    ipfs daemon &
+    # Run the IPFS daemon in background, initialize configs if necessary
+    ipfs daemon --init &
 
     # Wait for IPFS daemon to be ready
     while ! curl -s localhost:5001 > /dev/null
