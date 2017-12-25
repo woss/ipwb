339ce6c73aeae3bc197eaae43bb81fdf8816bb57
Sawood Alam <ibnesayeed@gmail.com>
2017-12-25 13:59:33 -0500

    Changing file name and commit message from deploy to release

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ echo $PYVAR\'$VERSION_STRING\'>'ipwb/'$FILE_NAME
 
 # Push to GitHub
 git add 'ipwb/'$FILE_NAME
-git commit -m "DEPLOY: Bump version for pypi to "$VERSION_STRING
+git commit -m "RELEASE: Bump version for pypi to "$VERSION_STRING
 
 # Create a tag in repo
 TAG_NAME='v'$VERSION_STRING
