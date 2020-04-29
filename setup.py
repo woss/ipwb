070ef18601648ca58d89e43968c7ca75a38d6533
Mat Kelly <me@matkelly.com>
2020-04-29 10:31:53 -0400

    Allow long line exception for temp fix for ipfshttpclient master instead of release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     ],
     install_requires=[
         'warcio>=1.5.3',
-        'ipfshttpclient @ git+https://github.com/ipfs-shipyard/py-ipfs-http-client@master',
+        'ipfshttpclient @ git+https://github.com/ipfs-shipyard/py-ipfs-http-client@master',  # noqa: E501
         'Flask==1.1.1',
         'pycryptodome>=3.4.11',
         'requests>=2.19.1',
