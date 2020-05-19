f4fe811006badebc4c241fd0543913437f3d673d
Sawood Alam <ibnesayeed@gmail.com>
2020-05-18 20:01:04 -0400

    Document the package release process

--- release.sh
+++ release.sh
@@ -20,3 +20,9 @@ TAG_NAME='v'$VERSION_STRING
 git tag $TAG_NAME
 git push
 git push origin $TAG_NAME
+
+# The `.github/workflows/dist.yml` Workflow is triggered automatically
+# when the repo is tagged by running this Shell script
+# or manually creating a relase in GH.
+# In either case, the Workflow will build the Python package
+# in GH's CI infrastructure and publish it to PyPI.
