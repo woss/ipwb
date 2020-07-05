e5ad8ef75a0bf1ba5455821fc85ac3058129ef82
Anatoly Scherbakov <altaisoft@gmail.com>
2020-07-06 00:28:25 +0700

    Tests for error_handler.exception_logger()

---
+++ tests/test_error_handler.py
@@ -0,0 +1,27 @@
+import pytest
+from unittest.mock import MagicMock, patch
+
+from ipwb.error_handler import exception_logger
+
+
+@exception_logger(catch=False)
+def reraised_error(arg):
+    raise Exception(arg)
+
+
+@exception_logger()
+def caught_error(arg):
+    raise Exception(arg)
+
+
+def test_re_raise():
+    with pytest.raises(Exception, match='foo'):
+        reraised_error('foo')
+
+
+def test_catch():
+    mock_logger = MagicMock()
+    with patch('ipwb.error_handler.logger.critical', mock_logger):
+        caught_error('boo')
+
+    assert mock_logger.called_once_with(('boo', ))
