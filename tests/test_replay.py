ac1904563bdd4720c1aae1d89070b5d3e698f1f9
Mat Kelly <me@matkelly.com>
2020-07-15 11:06:45 -0400

    Rm unused custom pytest mark

--- tests/test_replay.py
+++ tests/test_replay.py
@@ -61,7 +61,6 @@ def test_replay_search(warc, lookup, status, location):
     ipwbTest.stopReplay()
 
 
-@pytest.mark.testReplayDatedMemento
 def test_replay_dated_memento():
     ipwbTest.startReplay('salam-home.warc')
 
