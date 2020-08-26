bfeaa7b8600189e8f2804e98cf2ffd0d04428625
Mat Kelly <me@matkelly.com>
2020-08-26 10:00:08 -0400

    Rename index file variable to be more clear and to-style

--- ipwb/replay.py
+++ ipwb/replay.py
@@ -551,9 +551,9 @@ def all_exception_handler(error):
 def show_admin():
     status = {'ipwbVersion': ipwbVersion,
               'ipfsEndpoint': ipwb_utils.IPFSAPI_MUTLIADDRESS}
-    iFile = ipwb_utils.get_ipwb_replay_index_path()
+    index_file = ipwb_utils.get_ipwb_replay_index_path()
 
-    memento_info = calculate_memento_info_in_index(iFile)
+    memento_info = calculate_memento_info_in_index(index_file)
 
     mCount = memento_info['mementoCount']
     unique_urirs = len(memento_info['surt_uris'].keys())
@@ -561,7 +561,7 @@ def show_admin():
     oldest_datetime = memento_info['oldest_datetime']
     newest_datetime = memento_info['newest_datetime']
 
-    uris = get_uris_and_datetimes_in_cdxj(iFile)
+    uris = get_uris_and_datetimes_in_cdxj(index_file)
 
     # TODO: Calculate actual URI-R/M counts
     indexes = [{'path': ipwb_utils.get_ipwb_replay_index_path(),
@@ -582,18 +582,18 @@ def show_admin():
 
 @app.route('/', strict_slashes=False)
 def show_landing_page():
-    iFile = ipwb_utils.get_ipwb_replay_index_path()
-    memento_info = calculate_memento_info_in_index(iFile)
+    index_file = ipwb_utils.get_ipwb_replay_index_path()
+    memento_info = calculate_memento_info_in_index(index_file)
 
     mCount = memento_info['mementoCount']
     unique_urirs = len(memento_info['surt_uris'].keys())
     htmlCount = memento_info['htmlCount']
 
-    summary = {'index_path': iFile,
+    summary = {'index_path': index_file,
                'urimCount': mCount,
                'urirCount': unique_urirs,
                'htmlCount': htmlCount}
-    uris = get_uris_and_datetimes_in_cdxj(iFile)
+    uris = get_uris_and_datetimes_in_cdxj(index_file)
     return render_template('index.html', summary=summary, uris=uris)
 
 
