b4b1eb50841529d64b1e4b261dff4ec0a25d9fea
Florian Brandes <florian.brandes@posteo.de>
2020-10-09 12:33:16 +0200

    fix pylint errors in test

--- tests/test_daemon.py
+++ tests/test_daemon.py
@@ -1,13 +1,16 @@
 import pytest
 
+
 from ipwb import __main__
 from multiaddr import exceptions as multiaddr_exceptions
+
 
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
+
 
 daemon = None
 args = {
@@ -16,10 +19,11 @@ args = {
         "daemon_address": daemon,
         "debug": False,
         "e": False,
-        "outfile": None, 
-        "update_check": False, 
+        "outfile": None,
+        "update_check": False,
         "warc_path": ['../samples/warcs/sample-1.warc.gz']
     }
+
 
 def test_daemon_wrong_scheme():
     daemon = "/dnswrong/localhost/tcp/5001/http"
@@ -28,6 +32,7 @@ def test_daemon_wrong_scheme():
         __main__.checkArgs_index(dotdict(args))
         __main__.checkArgs_replay(dotdict(args))
 
+
 def test_daemon_wrong_ip():
     daemon = "/ip4/256.999.478.444/tcp/5001/http"
     args['daemon_address'] = daemon
@@ -35,12 +40,14 @@ def test_daemon_wrong_ip():
         __main__.checkArgs_index(dotdict(args))
         __main__.checkArgs_replay(dotdict(args))
 
+
 def test_daemon_wrong_protocol():
     daemon = "/dns/localhost/tcp/5001/httpwrong"
     args['daemon_address'] = daemon
     with pytest.raises(multiaddr_exceptions.StringParseError):
         __main__.checkArgs_index(dotdict(args))
         __main__.checkArgs_replay(dotdict(args))
+
 
 def test_daemon_wrong_format():
     daemon = "localhost:5001"
