7476b65bacb515092122fb81402c833c8fc128ad
Mat Kelly <me@matkelly.com>
2021-08-23 15:12:59 -0400

    Use simpler var name for URI

--- ipwb/replay.py
+++ ipwb/replay.py
@@ -521,7 +521,7 @@ def generate_cdxj_timemap_from_cdxj_lines(
 
     for i, line in enumerate(cdxj_lines):
         (surt_uri, datetime, json) = line.split(' ', 2)
-        unsurted_uri = unsurt(surt_uri)
+        uri = unsurt(surt_uri)
         dt_rfc1123 = ipwb_utils.digits14_to_rfc1123(datetime)
         first_last_str = ''
 
@@ -534,7 +534,7 @@ def generate_cdxj_timemap_from_cdxj_lines(
             first_last_str = 'first last '
 
         tm_data += (f'{datetime} {{'
-                    f'"uri": "{host_and_port}memento/{datetime}/{unsurted_uri}", '
+                    f'"uri": "{host_and_port}memento/{datetime}/{uri}", '
                     f'"rel": "{first_last_str}memento", '
                     f'"datetime"="{dt_rfc1123}"}}\n')
     return tm_data
